ae3cf88930809a7716195d29d93f051c4870961b
Diego Cardona <person.cardonadiego@gmail.com>
2021-11-01 11:08:10 -0500

    Hero section of index page basic structure

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,6 +6,28 @@ class IndexPage extends PureComponent {
     render () {
         return (
             <Fragment>
+                <div className="bg-blue-300 py-16 px-4">
+                    <button className="absolute top-4 right-0 bg-white py-1 px-4">Experiencias Naturales</button>
+                    <h1 className="text-6xl py-8">Xiaeou</h1>
+                    <div>
+                        <button className="bg-purple-400 hover:bg-purple-500 py-2 px-4 text-white">Comprar</button>
+                        <button className="bg-gray-100 hover:bg-gray-200 py-2 px-4">Rentar</button>
+                        <div className="bg-white p-4 grid grid-cols-3 gap-4">
+                            <div>
+                                <p>Ubicación</p>
+                                <p>Barrio Belén</p>
+                            </div>
+                            <div>
+                                <p>Inmueble</p>
+                                <p>Casa</p>
+                            </div>
+                            <div>
+                                <p>Precio</p>
+                                <p>$10.000.000 - $40.000.000 COP</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <h1 className="text-3xl">Resultados de tu busqueda</h1>
                 <div className="p-4 grid grid-cols-4 gap-4">
                     <div className="bg-gray-100 h-96 flex items-end">
